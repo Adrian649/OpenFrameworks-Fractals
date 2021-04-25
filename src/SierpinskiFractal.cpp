55bdc04605cf7e47cd128fe81bfb5d24276cfdd2
Francisco452 <francisco.jusino1@upr.edu>
2021-04-24 21:33:35 -0400

    Added Third Recursion

--- src/SierpinskiFractal.cpp
+++ src/SierpinskiFractal.cpp
@@ -23,6 +23,7 @@ void SierpinskiFractal::drawMode(int x, int y, int size, int n) {
 
     drawMode(x, y, size / 2, n - 1);
     drawMode((a.x + b.x) / 2, (a.y + b.y) / 2, size / 2, n - 1);
+    drawMode((a.x+c.x) / 2, (a.y+c.y) / 2, size / 2, n - 1);
 }
 
 void SierpinskiFractal::setActivate(bool a)
